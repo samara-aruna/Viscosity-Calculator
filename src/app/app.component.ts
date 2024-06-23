92d8e5ff5ee308692855de18c3552cedd910eb80
samara-aruna <asamarasinghe154@gmail.com>
2024-06-23 13:51:03 +0530

    Done HTML CSS Parts Have to do Type Scrip part and some HTML nad Css Parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'Fifth_Project';
+
+  public r_value!: number;
+  public p_value!: number;
+  public time_value!: number;
+  public Constant_value!: number;
+  public vessel_length!: number;
+
+  public result!: number;
 }
